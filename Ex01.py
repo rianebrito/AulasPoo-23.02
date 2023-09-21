5fb674f228893d31bb44e45b2f97ea12df140304
rianebrito <36201440+rianebrito@users.noreply.github.com>
2023-09-21 18:05:33 -0300

    Add files via upload

---
+++ Ex01.py
@@ -0,0 +1,4 @@
+num = int(input("Escreva um número inteiro:"))
+
+if num > 0:
+    print("O número é positivo!")
---
+++ Ex02.py
@@ -0,0 +1,7 @@
+nota1 = float(input("Escreva a nota 1:"))
+nota2 = float(input("Escreva a nota 2:"))
+
+media = (nota1 + nota2)/2
+
+if 3 <= media < 7:
+    print("Você está em RECUPERAÇÃO!!!")
